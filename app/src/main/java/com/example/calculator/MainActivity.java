920d0c5aa524a20aa695ee831b6abae398a1174f
Dark-Light-20 <sebatian.diaz@udenar.edu.co>
2019-10-02 22:49:19 -0500

    add about activity // add locales

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -89,6 +90,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.button_about:
+                Intent intent=new Intent(this, AboutActivity.class);
+                startActivity(intent);
                 break;
 
             default:
