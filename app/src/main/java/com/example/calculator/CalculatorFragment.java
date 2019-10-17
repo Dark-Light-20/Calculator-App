564befe5b5fae5cb0edae7047f7f7f2fd8a49923
Dark-Light-20 <sebastian.diaz@udenar.edu.co>
2019-10-17 17:26:40 -0500

    added fragments and nav_view

---
+++ app/src/main/java/com/example/calculator/CalculatorFragment.java
@@ -0,0 +1,153 @@
+package com.example.calculator;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import org.mariuszgromada.math.mxparser.Expression;
+
+public class CalculatorFragment extends Fragment implements View.OnClickListener {
+    private View view;
+    private double a = 0;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_calculator,
+                container, false);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
+            Button button_sen = view.findViewById(R.id.button_sen);
+            Button button_cos = view.findViewById(R.id.button_cos);
+            Button button_tan = view.findViewById(R.id.button_tan);
+            Button button_inv = view.findViewById(R.id.button_inv);
+            Button button_par_left = view.findViewById(R.id.button_par_left);
+            Button button_par_right = view.findViewById(R.id.button_par_right);
+            Button button_A = view.findViewById(R.id.button_A);
+            Button button_give_A = view.findViewById(R.id.button_give_A);
+            Button button_exp = view.findViewById(R.id.button_exp);
+
+            button_sen.setOnClickListener(this);
+            button_cos.setOnClickListener(this);
+            button_tan.setOnClickListener(this);
+            button_inv.setOnClickListener(this);
+            button_par_left.setOnClickListener(this);
+            button_par_right.setOnClickListener(this);
+            button_A.setOnClickListener(this);
+            button_give_A.setOnClickListener(this);
+            button_exp.setOnClickListener(this);
+        }
+
+
+        Button button0 = view.findViewById(R.id.button0);
+        Button button1 = view.findViewById(R.id.button1);
+        Button button2 = view.findViewById(R.id.button2);
+        Button button3 = view.findViewById(R.id.button3);
+        Button button4 = view.findViewById(R.id.button4);
+        Button button5 = view.findViewById(R.id.button5);
+        Button button6 = view.findViewById(R.id.button6);
+        Button button7 = view.findViewById(R.id.button7);
+        Button button8 = view.findViewById(R.id.button8);
+        Button button9 = view.findViewById(R.id.button9);
+        Button button_sum = view.findViewById(R.id.button_sum);
+        Button button_less = view.findViewById(R.id.button_less);
+        Button button_mul = view.findViewById(R.id.button_mul);
+        Button button_div = view.findViewById(R.id.button_div);
+        Button button_dot = view.findViewById(R.id.button_dot);
+        Button button_eq = view.findViewById(R.id.button_eq);
+        Button button_clear = view.findViewById(R.id.button_clear);
+
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button_sum.setOnClickListener(this);
+        button_less.setOnClickListener(this);
+        button_mul.setOnClickListener(this);
+        button_div.setOnClickListener(this);
+        button_dot.setOnClickListener(this);
+        button_eq.setOnClickListener(this);
+        button_clear.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        TextView text = view.findViewById(R.id.text_res);
+        String res = text.getText().toString();
+
+        switch (v.getId()){
+            case R.id.button_eq:
+                Expression expression = new Expression(res);
+                text.setText(String.valueOf(expression.calculate()));
+                return;
+
+            case R.id.button_clear:
+                text.setText("");
+                break;
+
+            case R.id.button_sen:
+                text.setText(res+="sin(");
+                break;
+
+            case R.id.button_cos:
+                text.setText(res+="cos(");
+                break;
+
+            case R.id.button_tan:
+                text.setText(res+="tan(");
+                break;
+
+            case R.id.button_inv:
+                text.setText(res+="(1/");
+                break;
+
+            case R.id.button_par_left:
+                text.setText(res+="(");
+                break;
+
+            case R.id.button_par_right:
+                text.setText(res+=")");
+                break;
+
+            case R.id.button_exp:
+                text.setText(res+="^(");
+                break;
+
+            case R.id.button_give_A:
+                if(!res.equals("")) a = Double.valueOf(res);
+                else a = 0;
+                break;
+
+            case R.id.button_A:
+                text.setText(res+=String.valueOf(a));
+                break;
+
+            default:
+                Button button = view.findViewById(v.getId());
+                res += button.getText().toString();
+                text.setText(res);
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.calculator;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
 import  org.mariuszgromada.math.mxparser.*;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     double a = 0;
 
     @Override
@@ -19,131 +27,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            getSupportActionBar().hide();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            Button button_sen = findViewById(R.id.button_sen);
-            Button button_cos = findViewById(R.id.button_cos);
-            Button button_tan = findViewById(R.id.button_tan);
-            Button button_inv = findViewById(R.id.button_inv);
-            Button button_par_left = findViewById(R.id.button_par_left);
-            Button button_par_right = findViewById(R.id.button_par_right);
-            Button button_A = findViewById(R.id.button_A);
-            Button button_give_A = findViewById(R.id.button_give_A);
-            Button button_exp = findViewById(R.id.button_exp);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-            button_sen.setOnClickListener(this);
-            button_cos.setOnClickListener(this);
-            button_tan.setOnClickListener(this);
-            button_inv.setOnClickListener(this);
-            button_par_left.setOnClickListener(this);
-            button_par_right.setOnClickListener(this);
-            button_A.setOnClickListener(this);
-            button_give_A.setOnClickListener(this);
-            button_exp.setOnClickListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawe_open, R.string.navigation_drawe_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new CalculatorFragment()).commit();
+            navigationView.setCheckedItem(R.id.calc_item);
         }
-
-        Button button0 = findViewById(R.id.button0);
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
-        Button button7 = findViewById(R.id.button7);
-        Button button8 = findViewById(R.id.button8);
-        Button button9 = findViewById(R.id.button9);
-        Button button_sum = findViewById(R.id.button_sum);
-        Button button_less = findViewById(R.id.button_less);
-        Button button_mul = findViewById(R.id.button_mul);
-        Button button_div = findViewById(R.id.button_div);
-        Button button_dot = findViewById(R.id.button_dot);
-        Button button_eq = findViewById(R.id.button_eq);
-        Button button_clear = findViewById(R.id.button_clear);
-        Button button_about = findViewById(R.id.button_about);
-
-        button0.setOnClickListener(this);
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
-        button5.setOnClickListener(this);
-        button6.setOnClickListener(this);
-        button7.setOnClickListener(this);
-        button8.setOnClickListener(this);
-        button9.setOnClickListener(this);
-        button_sum.setOnClickListener(this);
-        button_less.setOnClickListener(this);
-        button_mul.setOnClickListener(this);
-        button_div.setOnClickListener(this);
-        button_dot.setOnClickListener(this);
-        button_eq.setOnClickListener(this);
-        button_clear.setOnClickListener(this);
-        button_about.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        TextView text = findViewById(R.id.text_res);
-        String res = text.getText().toString();
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-        switch (v.getId()){
-            case R.id.button_eq:
-                Expression expression = new Expression(res);
-                text.setText(String.valueOf(expression.calculate()));
-                return;
-
-            case R.id.button_clear:
-                text.setText("");
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.calc_item:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CalculatorFragment()).commit();
                 break;
 
-            case R.id.button_about:
-                Intent intent=new Intent(this, AboutActivity.class);
-                startActivity(intent);
-                break;
-
-            case R.id.button_sen:
-                text.setText(res+="sin(");
-                break;
-
-            case R.id.button_cos:
-                text.setText(res+="cos(");
-                break;
-
-            case R.id.button_tan:
-                text.setText(res+="tan(");
-                break;
-
-            case R.id.button_inv:
-                text.setText(res+="(1/");
-                break;
-
-            case R.id.button_par_left:
-                text.setText(res+="(");
-                break;
-
-            case R.id.button_par_right:
-                text.setText(res+=")");
-                break;
-
-            case R.id.button_exp:
-                text.setText(res+="^(");
-                break;
-
-            case R.id.button_give_A:
-                if(!res.equals("")) a = Double.valueOf(res);
-                else a = 0;
-                break;
-
-            case R.id.button_A:
-                text.setText(res+=String.valueOf(a));
-                break;
-
-            default:
-                Button button = findViewById(v.getId());
-                res += button.getText().toString();
-                text.setText(res);
+            case R.id.about_item:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AboutFragment()).commit();
                 break;
         }
+
+        return true;
     }
 }
